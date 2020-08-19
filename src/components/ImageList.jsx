1e56ad072154d3b52c104fd57e754932d24a3f46
Hassan Mian <mian@willandskill.se>
2020-08-19 11:18:37 +0200

    Add logic for fetching images

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 import ImageCard from './ImageCard'
 
 export default function ImageList({data}) {
-    
+    let [imageList, setImageList] = useState([])
+
+    useEffect(() => {
+        fetchImageList()
+    }, [])
+
+    function fetchImageList() {
+        fetch("https://image-mock-data.firebaseio.com/images.json")
+            .then(res => res.json())
+            .then((result) => {
+                setImageList(result)
+            })
+    }
     return (
         <div className="row">
         {
-            data.map((item, index) => {
+            imageList.map((item, index) => {
                 return <ImageCard key={index} title={ item.title } description={ item.description } imageURL={item.imageURL} />
             })  
         }
